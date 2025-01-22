e6530043e394387596ae99aac448896ff4f90e74
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-23 01:23:23 +0330

    Defined a single point

---
+++ src/core/point.cpp
@@ -0,0 +1,189 @@
+#include "point.h"
+
+Point &Point::operator=(Point const &_object)
+{
+    this->_x = _object._x;
+    this->_y = _object._y;
+    return *this;
+}
+
+Unit Point::GetX() const
+{
+    return this->_x;
+}
+
+Unit &Point::ReferX()
+{
+    return this->_x;
+}
+
+void Point::Traspose()
+{
+    Unit _temp = this->_x;
+    this->_x = this->_y;
+    this->_y = _temp;
+}
+
+void Point::SetX(Unit const &_x)
+{
+    this->_x = _x;
+}
+
+Unit Point::GetY() const
+{
+    return this->_y;
+}
+
+Unit &Point::ReferY()
+{
+    return this->_y;
+}
+
+void Point::SetY(Unit const &_y)
+{
+    this->_y = _y;
+}
+
+void Point::MoveTop()
+{
+    MoveTop(1);
+}
+void Point::MoveTop(Unit const &_factor)
+{
+    this->_y -= _factor;
+}
+
+void Point::MoveLeft()
+{
+    MoveLeft(1);
+}
+
+void Point::MoveLeft(Unit const &_factor)
+{
+    this->_x -= _factor;
+}
+
+void Point::MoveRight()
+{
+    MoveRight(1);
+}
+
+void Point::MoveRight(Unit const &_factor)
+{
+    this->_x += _factor;
+}
+
+void Point::MoveBottom()
+{
+    MoveBottom(1);
+}
+
+void Point::MoveBottom(Unit const &_factor)
+{
+    this->_y += _factor;
+}
+
+void Point::MoveTopLeft()
+{
+    MoveTopLeft(1);
+}
+
+void Point::MoveTopLeft(Unit const &_factor)
+{
+    MoveTop(_factor);
+    MoveLeft(_factor);
+}
+
+void Point::MoveTopRight()
+{
+    MoveTopRight(1);
+}
+
+void Point::MoveTopRight(Unit const &_factor)
+{
+    MoveTop(_factor);
+    MoveRight(_factor);
+}
+
+void Point::MoveBottomLeft()
+{
+    MoveBottomLeft(1);
+}
+
+void Point::MoveBottomLeft(Unit const &_factor)
+{
+    MoveBottom(_factor);
+    MoveLeft(_factor);
+}
+
+void Point::MoveBottomRight()
+{
+    MoveBottomRight(1);
+}
+
+void Point::MoveBottomRight(Unit const &_factor)
+{
+    MoveBottom(_factor);
+    MoveRight(_factor);
+}
+
+Point &Point::operator+=(Point const &point)
+{
+    this->_x += point._x;
+    this->_y += point._y;
+    return *this;
+}
+
+Point &Point::operator-=(Point const &point)
+{
+    this->_x -= point._x;
+    this->_y -= point._y;
+    return *this;
+}
+
+Point Point::Transposed(Point const &point)
+{
+    return Point(point._y, point._x);
+}
+
+Point operator+(Point const &one, Point const &two)
+{
+    return Point(one._x + two._x, one._y + two._y);
+}
+
+Point operator-(Point const &one, Point const &two) 
+{
+    return Point(one._x - two._x, one._y - two._y);
+}
+
+bool operator==(Point const &one, Point const &two)
+{
+    return (one._x == two._x) && (one._y == two._y);
+}
+
+bool operator!=(Point const &one, Point const &two)
+{
+    return !(one == two);
+}
+
+std::ostream& operator<<(std::ostream &output, Point const& point)
+{
+    output << '(' << point._x << ", " << point._y << ')';
+    return output;
+}
+
+using Byte = char;
+std::ostream& Point::ToBinary(std::ostream &stream)
+{
+    stream.write(reinterpret_cast<const Byte*>(&this->_x), sizeof(Unit));
+    stream.write(reinterpret_cast<const Byte*>(&this->_y), sizeof(Unit));
+    return stream;
+}
+std::istream& Point::FromBinary(std::istream &stream)
+{
+    stream.read(reinterpret_cast<Byte*>(&this->_x), sizeof(Unit));
+    stream.read(reinterpret_cast<Byte*>(&this->_y), sizeof(Unit));
+    return stream;
+}
+
+
---
+++ src/core/point.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <cstdint>
+#include "interfaces.h"
+
+using Unit = int64_t;
+
+class Point : public ISerialize
+{
+private:
+    Unit _x = 0;
+    Unit _y = 0;
+
+private:
+    friend Point operator+(Point const &one, Point const &two); 
+    friend Point operator-(Point const &one, Point const &two);
+
+    friend bool operator==(Point const &one, Point const &two);
+    friend bool operator!=(Point const &one, Point const &two);
+    friend std::ostream& operator<<(std::ostream &output, Point const& point);
+
+public:
+    Point();
+    Point(Point const& _object);
+    Point(Unit const& _x, Unit const& _y);
+
+    Point& operator=(Point const& _object);
+
+    Unit GetX() const;
+    Unit& ReferX();
+
+    Unit GetY() const;
+    Unit& ReferY();
+
+    void Traspose();
+    void SetX(Unit const& _x);
+    void SetY(Unit const& _y);
+
+    void MoveTop();
+    void MoveTop(Unit const& _factor);
+
+    void MoveLeft();
+    void MoveLeft(Unit const& _factor);
+
+    void MoveRight();
+    void MoveRight(Unit const& _factor);
+
+    void MoveBottom();
+    void MoveBottom(Unit const& _factor);
+
+    void MoveTopLeft();
+    void MoveTopLeft(Unit const& _factor);
+
+    void MoveTopRight();
+    void MoveTopRight(Unit const& _factor);
+
+    void MoveBottomLeft();
+    void MoveBottomLeft(Unit const& _factor);
+
+    void MoveBottomRight();
+    void MoveBottomRight(Unit const& _factor);
+
+    Point& operator+=(Point const& point);
+    Point& operator-=(Point const& point);
+
+public:
+    static Point Transposed(Point const &point);
+    virtual std::ostream& ToBinary(std::ostream &stream) override;
+    virtual std::istream& FromBinary(std::istream &stream) override;
+};
+
+Point operator+(Point const &one, Point const &two); 
+Point operator-(Point const &one, Point const &two);
+
+bool operator==(Point const &one, Point const &two);
+bool operator!=(Point const &one, Point const &two);
+std::ostream& operator<<(std::ostream &output, Point const& point);

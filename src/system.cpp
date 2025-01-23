479a19cce9dd5c29200e2bcef3de31408718a638
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-23 14:20:42 +0330

    Added a function to get an instant key

--- src/system.cpp
+++ src/system.cpp
@@ -9,3 +9,38 @@ void System::EraseConsole() {
 void System::Delay(int _duration) {
     Sleep(static_cast<DWORD>(_duration));
 }
+
+System::KeyNumber System::InstantKey()
+{
+    HANDLE input = GetStdHandle(STD_INPUT_HANDLE);
+    if (input == INVALID_HANDLE_VALUE)
+    {
+        return -1;
+    }
+
+    INPUT_RECORD irInBuf;
+    DWORD dwMode, cNumRead;
+    if (!GetConsoleMode(input, &dwMode))
+    {
+        return -1;
+    }
+    
+    dwMode &= ~ENABLE_ECHO_INPUT;
+    dwMode &= ~ENABLE_LINE_INPUT;
+    if (!SetConsoleMode(input, dwMode))
+    {
+        return -1;
+    }
+
+    while (true) 
+    {
+        if (!ReadConsoleInput(input, &irInBuf, 1, &cNumRead))
+        {
+            return -1;
+        }
+        else if (irInBuf.EventType == KEY_EVENT && irInBuf.Event.KeyEvent.bKeyDown)
+        {
+            return irInBuf.Event.KeyEvent.wVirtualKeyCode;
+        }
+    }
+}
--- src/system.h
+++ src/system.h
@@ -1,10 +1,15 @@
 #pragma once
+#include <cstdint>
 
 namespace System {
+    using KeyNumber = int32_t;
 
     // Erases everything from the screen.
     void EraseConsole();
 
     // Makes a pause in the execution flow.
     void Delay(int _duration);
+
+    // Gets an instant key from the user.
+    System::KeyNumber InstantKey();
 }

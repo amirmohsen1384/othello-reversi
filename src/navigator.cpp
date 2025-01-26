8d29bd82e7d822662cfdaaea3b4d618dc0d90aa7
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-01-26 17:42:44 +0330

    Improved the abstract navigator

---
+++ src/navigator.cpp
@@ -0,0 +1,79 @@
+#include "navigator.h"
+#include <windows.h>
+
+System::KeyNumber AbstractNavigator::GetKeyBottom() const {
+    return VK_DOWN;
+}
+
+System::KeyNumber AbstractNavigator::GetKeyTop() const {
+    return VK_UP;
+}
+
+System::KeyNumber AbstractNavigator::GetKeyLeft() const {
+    return VK_LEFT;
+}
+
+System::KeyNumber AbstractNavigator::GetKeyRight() const {
+    return VK_RIGHT;
+}
+
+System::KeyNumber AbstractNavigator::GetKeyAccept() const {
+    return VK_RETURN;
+}
+
+System::KeyNumber AbstractNavigator::GetKeyReject() const {
+    return VK_ESCAPE;
+}
+
+void AbstractNavigator::Accept() {
+    this->SetCurrentState(State::Accepted);
+}
+
+void AbstractNavigator::Reject() {
+    this->SetCurrentState(State::Rejected);
+}
+
+System::KeyNumber AbstractNavigator::GetCurrentKey() const {
+    return _currentKey;
+}
+
+void AbstractNavigator::ResetCurrentState()
+{
+    _currentState = State::Unknown;
+}
+
+State AbstractNavigator::GetCurrentState() const
+{
+    return _currentState;
+}
+
+void AbstractNavigator::SetCurrentState(State const& state) {
+    _currentState = state;
+}
+
+State AbstractNavigator::Execute() {
+    // Waits for the user for an instant key.
+    _currentKey = System::InstantKey();
+
+    // Checks the entered instant key.
+    if(_currentKey == this->GetKeyTop()) {
+        MoveTop();
+    }
+    else if(_currentKey == this->GetKeyLeft()) {
+        MoveLeft();
+    }
+    else if(_currentKey == this->GetKeyReject()) {
+        Reject();
+    }
+    else if(_currentKey == this->GetKeyRight()) {
+        MoveRight();
+    }
+    else if(_currentKey == this->GetKeyAccept()) {
+        Accept();
+    }
+    else if(_currentKey == this->GetKeyBottom()) {
+        MoveBottom();
+    }
+    
+    return this->GetCurrentState();
+}
---
+++ src/navigator.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "system.h"
+
+// Describes different states of a single navigator
+enum class State {
+    Unknown = 0,
+    Accepted = Unknown + 3,
+    Rejected = Unknown + 1,
+    UpdateNavigator = Unknown + 2,
+    InvalidKeyPressed = Unknown - 1
+};
+
+// Provides an interface for handling arrow keys.
+class AbstractNavigator {
+private:
+    State _currentState = State::Unknown;
+    System::KeyNumber _currentKey = 0;
+
+protected:
+    virtual System::KeyNumber GetKeyAccept() const;
+    virtual System::KeyNumber GetKeyReject() const;
+    virtual System::KeyNumber GetKeyBottom() const;
+    virtual System::KeyNumber GetKeyRight() const;
+    virtual System::KeyNumber GetKeyLeft() const;
+    virtual System::KeyNumber GetKeyTop() const;
+
+public:
+    virtual void MoveBottom() = 0;
+    virtual void MoveRight() = 0;
+    virtual void MoveLeft() = 0;
+    virtual void MoveTop() = 0;
+    virtual void Accept();
+    virtual void Reject();
+
+public:
+    void ResetCurrentState();
+    State GetCurrentState() const;
+    void SetCurrentState(State const& state);
+
+    virtual State Execute();
+    System::KeyNumber GetCurrentKey() const;
+};

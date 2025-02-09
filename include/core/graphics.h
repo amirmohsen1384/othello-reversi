e3fe160bf0f92740588d4133da63fe9854455cc7
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-02-09 13:25:59 +0330

    Added some pre-defined colors for certain purposes

--- include/core/graphics.h
+++ include/core/graphics.h
@@ -26,6 +26,15 @@ namespace Graphics {
 
     // Prints a string with the specified color.
     void Draw(std::string const& text, Color const &color);
+
+    // Describes some pre-defined colors for certain purposes.
+    enum class Action {
+        Blank = Color::Gray,
+        User = Color::BrightBlue,
+        Opponent = Color::BrightRed,
+        Legal = Color::BrightMagenta
+    };
+    
 }
 
 static constexpr Graphics::Color _default = Graphics::Color::White;
